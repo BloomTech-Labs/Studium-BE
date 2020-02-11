26b84883b83879cab57222c42ec24ed21194feee
Chris Cruz <christopherc1331@gmail.com>
2020-02-10 19:42:35 -0800

    Removed findByProducts model function

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,23 +1,24 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
-
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 module.exports = (req, res, next) => {
-
   const token = req.headers.authorization;
   if (token) {
-    jwt.verify(token, secrets.jwtSecret, (error, decodedToken ) => {
-      if(error) {
-        res.status(401).json({message:'This token has not been verified. You shall not pass...'})
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({
+          message: "This token has not been verified. You shall not pass..."
+        });
       } else {
         req.decodedJwt = decodedToken;
         next();
       }
-    })
+    });
   } else {
-    res.status(401).json({message:'You do not have an authorized token. Go away !'})
+    res
+      .status(401)
+      .json({ message: "You do not have an authorized token. Go away !" });
   }
-}
+};
