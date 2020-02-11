cfb1527b00c2003e1bb62154bdb23b04246ac630
Chris Cruz <christopherc1331@gmail.com>
2020-02-10 19:46:27 -0800

    Added default server port -5000

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const {PORT} = process.env ;
+const { PORT } = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
