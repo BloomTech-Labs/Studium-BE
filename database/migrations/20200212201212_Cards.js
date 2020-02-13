ff7c3608005b183dd2cc8b86624172af6598f74a
Chris Cruz <christopherc1331@gmail.com>
2020-02-12 20:38:48 -0800

    Added Cards migration table

--- database/migrations/20200212201212_Cards.js
+++ database/migrations/20200212201212_Cards.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cards", tbl => {
+    tbl.increments("card_id");
+
+    tbl
+      .integer("deck_id")
+      .notNullable()
+      .references("decks.deck_id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+
+    tbl.string("question").notNullable();
+
+    tbl.string("answer").notNullable();
+
+    tbl.text("tags");
+
+    tbl.string("background");
+
+    tbl.string("text");
+
+    tbl.blob("image_front");
+
+    tbl.blob("image_back");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cards");
 };
