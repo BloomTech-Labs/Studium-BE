9fff7dbf896108954a8d793efb1da0aff76ccfdf
Chris Cruz <christopherc1331@gmail.com>
2020-02-12 18:32:31 -0800

    First commit

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,45 @@
 // Update with your config settings.
-
+// database init
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/synaps.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/synaps.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     }
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
