0564ad33d86b46060d0cdd715c56012569b099ea
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-03-05 18:54:44 -0700

    Added try catch block for test.

--- routes/auth/registerRouter.spec.js
+++ routes/auth/registerRouter.spec.js
@@ -5,32 +5,36 @@ const cleaner = require( "knex-cleaner" );
 const request = supertest( app );
 
 describe( "Sample Test", () => {
-  it( "should test that true === true", () => {
-    expect( true ).toBe( true );
-  } );
+    it( "should test that true === true", () => {
+        expect( true ).toBe( true );
+    } );
 } );
 
 describe( "Register Endpoints", () => {
-  beforeAll( () => {
-    cleaner.clean( knex );
-  } );
-  
-  it( "should create a new user", ( done ) => {
-    request
-      .post( "/api/register" )
-      .send( { uid: "1234fjkdsl", username: "testing123" } )
-      .expect( 201 )
-      .then( res => {
-        const user = res.body;
-        expect( user ).toEqual( {
-          user_id: expect.any( Number ),
-          uid: "1234fjkdsl",
-          username: "testing123",
-          created_at: expect.any( String ),
-          updated_at: expect.any( String )
-        } );
-        done();
-      } );
+    beforeAll( () => {
+        cleaner.clean( knex );
+    } );
     
-  } );
+    it( "should create a new user", ( done ) => {
+        try{
+            request
+                .post( "/api/register" )
+                .send( { uid: "1234fjkdsl", username: "testing123" } )
+                .expect( 201 )
+                .then( res => {
+                    const user = res.body;
+                    expect( user ).toEqual( {
+                        user_id: expect.any( Number ),
+                        uid: "1234fjkdsl",
+                        username: "testing123",
+                        created_at: expect.any( String ),
+                        updated_at: expect.any( String ),
+                    } );
+                    done();
+                } );
+        }catch( e ){
+            console.log( e );
+        }
+        
+    } );
 } );
