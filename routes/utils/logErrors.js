b940560f7a3ca63e905f4c7eb8140b48cd4c1f97
Anthony Lopez <53539912+AnthonyLopez1120@users.noreply.github.com>
2020-05-26 20:22:23 -0500

    Delete logErrors.js

--- routes/utils/logErrors.js
+++
@@ -1,7 +0,0 @@
-module.exports = ( err, req, res, next ) => {
-  res.logger.error( "Came from: " + err.functionName || "Unknown",
-    err.stack || err.userMessage || "Server error"
-  );
-  next( err );
-  
-};
