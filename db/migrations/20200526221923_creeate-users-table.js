a0a31927b9c49501189f81f0bc068767a9cb7917
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-05-26 23:01:38 -0400

    Created database, created users table

---
+++ db/migrations/20200526221923_creeate-users-table.js
@@ -0,0 +1,34 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', tbl => {
+        tbl.increments(); // ID
+        tbl.string("first_name") // FIRST NAME
+            .notNullable();
+        tbl.string("last_name") // LAST NAME
+            .notNullable();
+        tbl.string("username", 255) // USERNAME
+            .notNullable()
+            .unique();
+        tbl.string("email") // EMAIL
+            .notNullable()
+            .unique();
+        tbl.string("password", 255) // PASSWORD
+            .notNullable();
+        tbl.integer("user_points") // POINTS
+            .notNullable()
+            .defaultTo(0);
+        tbl.integer("user_streak") // STREAK 
+            .notNullable()
+            .defaultTo(0);
+        tbl.integer("user_level") // LEVEL
+            .notNullable()
+            .defaultTo(1);
+        tbl.timestamp("created_at", { useTz: false }); // CREATED AT TIMESTAMP 
+        tbl.string("user_img"); // USER AVATAR/IMG 
+    })
+
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
+};
