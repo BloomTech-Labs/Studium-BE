8fa43af743c68ad6b5ce8c1ef83d627ddf06f92c
Anthony Lopez <53539912+AnthonyLopez1120@users.noreply.github.com>
2020-05-26 20:22:03 -0500

    Delete finalErrorHandler.js

--- routes/utils/finalErrorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = ( err, req, res, next ) => {
-  res.logger( "Setting website error message." );
-  res.logger( "Waiting for next request." );
-  res.status( err.message || 500 ).json( {
-    status: err.status || 500, message: err.userMessage || "Server error"
-  } );
-};
