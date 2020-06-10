d87514ade623031feb6cc9c3311b92726f2eff1d
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-06-09 20:52:36 -0400

    Modified server.js to allow sessions, tags, and cards routers to be used

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ const usersRouter = require('../resources/users/users-router.js')
 const decksRouter = require('../resources/decks/decks-router.js')
 const cardsRouter = require('../resources/cards/cards-router.js')
 const sessionsRouter = require('../resources/sessions/sessions-router.js')
+const tagsRouter = require('../resources/tags/tags-router.js')
 // --> insert auth middleware path(s) here
 
 const server = express()
@@ -16,8 +17,9 @@ server.use(cors())
 
 server.use('/api/users', usersRouter) // --> missing auth middleware
 server.use('/api/decks', decksRouter) // --> missing auth middleware
-// server.use('/api/cards', cardsRouter) // --> missing auth middleware
-// server.use('/api/sessions', sessionsRouter) // --> missing auth middleware
+server.use('/api/cards', cardsRouter) // --> missing auth middleware
+server.use('/api/sessions', sessionsRouter) // --> missing auth middleware
+server.use('/api/tags', tagsRouter) // --> missing auth middleware
 
 server.get('/', (req, res) => {
    res.send('The api is up.')
