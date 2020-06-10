742b697f48ba8e28c59b0a8ebd474c77c27c56f6
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-06-09 20:05:05 -0400

    Added cards model & did get request for cards

--- resources/cards/cards-router.js
+++ resources/cards/cards-router.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../cards/cards-model.js')
+const db = require('./cards-model.js')
 
+
+// GET ALL CARDS
+router.get('/', (req, res) => {
+    db.find()
+        .then(cards => {
+            res.json(cards)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "There was an error retrieving the cards!" })
+        })
+})
+
+// GET SPECIFIC CARD 
 router.get("/:id", (req, res) => {
     db.getCardbyId(req.params.id)
         .then(card => {
-            card.forEach(card => {
-                db.getCardTags(req.params.id).then(tags => {
-                    card.tags = tags;
-                    res.status(201).json(card)
+            if (card.length) {
+                card.forEach(card => {
+                    db.getCardTags(req.params.id).then(tags => {
+                        card.tags = tags;
+                        res.status(201).json(card)
+                    })
                 })
-            })
+            } else {
+                res.status(404).json({ errorMessage: "No such card with that ID exists." })
+            }
 
         })
         .catch(err => {
