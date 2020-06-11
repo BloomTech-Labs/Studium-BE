0687611b00fef76c6019d327d116ade719eafac0
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-06-10 20:52:37 -0400

    Test database created

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,28 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './db/studium_test.db3',
+    },
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/production'
+      directory: './db/seeds/test'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
     },
     useNullAsDefault: true
   },
 
   test: {
     client: 'sqlite3',
-    connection: { filename: './database/studium_test.db3' },
+    connection: { filename: './db/studium_test.db3' },
     migrations: {
       directory: './db/migrations'
     },
