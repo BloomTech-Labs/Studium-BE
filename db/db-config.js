0bb74407dafa0b103b2b92db36b71b3a7ed79fd9
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-06-11 23:10:15 -0400

    Fixed environment in dbconfig

--- db/db-config.js
+++ db/db-config.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const knex = require('knex')
 const config = require('../knexfile.js')
 
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || 'production';
 
 module.exports = knex(config[environment])
