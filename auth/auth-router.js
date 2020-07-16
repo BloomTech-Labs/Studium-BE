7579c2226e8e7d92c61fb78a96355b13d4e6138e
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-07-15 22:23:56 -0400

    updated registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,7 +21,16 @@ router.post('/register', (req, res) => {
 
   User.add(user)
     .then(newUser =>{
-      res.status(201).json(newUser)
+      if (user) {
+        const token = generateToken(user) 
+        res.status(200).json({ 
+         message: `Welcome ${user.username}`, 
+         token, 
+         user
+       })
+     } else {
+         res.status(401).json({ message: "Invalid registration, try again" })
+     }
     })
     .catch(err =>{
       res.status(500).json({message: "Error with adding user", err})
