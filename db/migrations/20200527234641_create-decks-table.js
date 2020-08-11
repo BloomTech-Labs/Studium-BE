aba1924ad113937336fac8fcf259986fbb07194b
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-08-10 22:03:53 -0400

    Fixes async and uuid

--- db/migrations/20200527234641_create-decks-table.js
+++ db/migrations/20200527234641_create-decks-table.js
@@ -1,7 +1,9 @@
 
-exports.up = function (knex) {
+exports.up = function async(knex) {
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     return knex.schema.createTable('decks', tbl => {
-        tbl.uuid('id').notNullable().primary(); // ID
+        tbl.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).notNullable().primary(); // ID
         tbl.uuid('user_id') // USER ID FROM USERS TABLE 
             .unsigned()
             .notNullable()
@@ -20,5 +22,6 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
+    knex.raw('drop extension if exists "uuid-ossp"');
     return knex.schema.dropTableIfExists('decks');
 };
