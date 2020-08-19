9e20c973946eb2b825efa1f21edefa1a8c2b7471
juniordugue <jrdugue@gmail.com>
2020-08-18 22:26:53 -0400

    added tests for uuid

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
 let request = require("supertest");
-let server = require('./api/server');
+let server = require("./api/server");
 
-describe('Test the root path', () => {
-  test('It should respond to the GET method', async () => {
-    const res = await request(server)
-      .get("/");
+
+describe("Test the root path", () => {
+  test("It should respond to the GET method", async () => {
+    const res = await request(server).get("/");
 
     expect(res.statusCode).toBe(200);
   });
--- resources/users/users-router.js
+++ resources/users/users-router.js
@@ -1,104 +1,97 @@
-const express = require('express')
+const express = require("express");
 
-const Users = require('./users-model.js')
+const Users = require("./users-model.js");
 
-const router = express.Router()
+const router = express.Router();
 
-const restricted = require('../../auth/authenticate-middleware')
+const restricted = require("../../auth/authenticate-middleware");
 
-router.get('/', (req, res) => {
-   Users.find()
-      .then(users => {
-         res.json(users)
-      })
-      .catch(err => {
-         res.status(500).json({ message: 'Failed to retrieve users' })
-      })
-})
+router.get("/", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to retrieve users" });
+    });
+});
 
 router.get("/me", restricted, (req, res) => {
-   Users.findById(req.decodedToken.subject)
-      .then(user => {
-         delete user.password;
-         res.status(201).json(user)
-      })
-      .catch(err => {
-         res.status(500).json({ errMessage: "Failed to get user by ID" })
-      })
-})
+  Users.findById(req.decodedToken.subject)
+    .then((user) => {
+      delete user.password;
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Failed to get user by ID" });
+    });
+});
 
-router.get('/:id', (req, res) => {
-   const { id } = req.params
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-   Users.findById(id)
-      .then(user => {
-         user
-            ? res.json(user)
-            : res.status(404).json({ message: 'Could not find a user with the given id' })
-      })
-      .catch(err => {
-         res.status(500).json({ message: 'Failed to get user' });
-      })
-})
+  Users.findById(id)
+    .then((user) => {
+      user ? res.json(user) : res.status(404).json({ message: "Could not find a user with the given id" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get user" });
+    });
+});
 
-router.get('/:id/decks', (req, res) => {
-   const { id } = req.params;
+router.get("/:id/decks", (req, res) => {
+  const { id } = req.params;
 
-   Users.findDeckByUserId(id)
-      .then(data => {
-         data ?
-            res.status(200).json(data) :
-            res.status(404).json({ errMessage: "Cannot find user data." })
-      })
-      .catch(err => {
-         res.status(500).json({ errMessage: "Unable to retrieve decks by User ID." })
-      })
-})
+  Users.findDeckByUserId(id)
+    .then((data) => {
+      data ? res.status(200).json(data) : res.status(404).json({ errMessage: "Cannot find user data." });
+    })
+    .catch((err) => {
+      res.status(500).json({ errMessage: "Unable to retrieve decks by User ID." });
+    });
+});
 
-router.post('/', (req, res) => {
-   const userData = req.body
+router.post("/", (req, res) => {
+  const userData = req.body;
 
-   Users.add(userData)
-      .then(user => {
-         res.status(201).json(user)
-      })
-      .catch(err => {
-         res.status(500).json({ message: 'Failed to create new user' });
-      });
-})
+  Users.add(userData)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create new user" });
+    });
+});
 
-router.put('/:id', (req, res) => {
-   const { id } = req.params
-   const changes = req.body
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
 
-   Users.findById(id)
-      .then(user => {
-         if (user) {
-            Users.update(changes, id)
-               .then(updatedUser => {
-                  res.json(updatedUser)
-               })
-         } else {
-            res.status(404).json({ message: 'Could not find a user with the given id' })
-         }
-      })
-      .catch(err => {
-         res.status(500).json({ message: 'Failed to update user' })
-      })
-})
+  Users.findById(id)
+    .then((user) => {
+      if (user) {
+        Users.update(changes, id).then((updatedUser) => {
+          res.json(updatedUser);
+        });
+      } else {
+        res.status(404).json({ message: "Could not find a user with the given id" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update user" });
+    });
+});
 
-router.delete('/:id', (req, res) => {
-   const { id } = req.params
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
 
-   Users.remove(id)
-      .then(deleted => {
-         deleted
-            ? res.json({ removed: deleted })
-            : res.status(404).json({ message: 'Could not find a user with the given id' })
-      })
-      .catch(err => {
-         res.status(500).json({ message: 'Failed to delete user' });
-      });
-})
+  Users.remove(id)
+    .then((deleted) => {
+      deleted ? res.json({ removed: deleted }) : res.status(404).json({ message: "Could not find a user with the given id" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete user" });
+    });
+});
 
-module.exports = router
+module.exports = router;
